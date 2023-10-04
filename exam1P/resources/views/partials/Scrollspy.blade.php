a9bde04d08f9fcb37ec8bb78ff1cbf8503f32e0b
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2023-10-04 16:39:47 -0600

    se agregaron las rutas al Scrollspy

--- exam1P/resources/views/partials/Scrollspy.blade.php
+++ exam1P/resources/views/partials/Scrollspy.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
   <div class="col-4">
     <div id="list-example" class="list-group">
-      <a class="list-group-item list-group-item-action" href="#list-item-1">Item 1</a>
-      <a class="list-group-item list-group-item-action" href="#list-item-2">Item 2</a>
-      <a class="list-group-item list-group-item-action" href="#list-item-3">Item 3</a>
-      <a class="list-group-item list-group-item-action" href="#list-item-4">Item 4</a>
+      <a class="list-group-item list-group-item-action" href="/">Item 1</a>
+      <a class="list-group-item list-group-item-action" href="/vista1">Item 2</a>
+      <a class="list-group-item list-group-item-action" href="/vista2">Item 3</a>
+      <a class="list-group-item list-group-item-action" href="/vista3">Item 4</a>
     </div>
   </div>
   <div class="col-8">
